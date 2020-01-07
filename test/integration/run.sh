65ad9f6ebcaf78d0d03b6124f28e13d74be5b0a1
d-kuro <kurosawa7620@gmail.com>
2020-01-08 01:05:13 +0900

    Remove old CRD manifest.

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -8,7 +8,7 @@ kubectl create -f deploy/role.yaml
 kubectl create -f deploy/role_binding.yaml
 
 # Setup the CRD
-kubectl create -f deploy/crds/mumoshu_v1alpha1_awssecret_crd.yaml
+kubectl create -f deploy/crds/mumoshu.github.io_awssecrets_crd.yaml
 
 # Deploy the app-operator
 cat deploy/operator.yaml | sed -e 's/REPLACE_THIS_WITH_YOUR_REGION/ap-northeast-1/' | kubectl create -f -
